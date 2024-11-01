2d2bbf4330f7617b5a98c09eed69ef4d62890c52
quinnw10 <jquinnw10@gmail.com>
2024-11-01 16:09:58 -0400

    adding initial component structure

---
+++ react-portfolio/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+// src/components/Footer.jsx
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Connect with me on:</p>
+      <a href="https://github.com/yourusername">GitHub</a>
+      <a href="https://linkedin.com/in/yourusername">LinkedIn</a>
+      <a href="https://twitter.com/yourusername">Twitter</a>
+    </footer>
+  );
+};
+
+export default Footer;
